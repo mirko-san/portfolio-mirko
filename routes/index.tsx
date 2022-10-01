9934e2ef009b9e34de79114101e8e41b70abd4ea
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-10-01 20:09:00 +0900

    :sparkles: ホーム画面を大幅に書き換え

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,48 @@
-import Counter from "../islands/Counter.tsx";
+import { Head } from "$fresh/src/runtime/head.ts";
 
 export default function Home() {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
-      <img
-        src="/logo.svg"
-        class="w-32 h-32"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class="my-6">
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+      <Head>
+        <link rel="stylesheet" href="/main.css" />
+      </Head>
+
+      <h2 class="text-5xl text-center mt-8">Profile</h2>
+      <section>
+        <div class="flex justify-center mt-8">
+          <img
+            class="bg-white max-w-[80%] rounded-full object-fill border-8 border-secondary"
+            src="/profile_image.png"
+            alt="mirko-san profile image"
+          />
+        </div>
+
+        <div class="flex justify-center mt-8">
+          <p class="text-5xl">
+            <span>ミルコ ( mirko )</span>
+            <span class="mirko-icon pl-2">&#xf0000;</span>
+          </p>
+        </div>
+
+        <div class="flex justify-center mt-4">
+          <ul class="list-disc">
+            <li>
+              <span>セルフ受肉勢の VTuber です。</span>
+            </li>
+            <li>
+              <span>普段はフロントエンドエンジニアとして働いています。</span>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section>
+        <div class="flex justify-center mt-4">
+          <div class="bg-primary rounded inline-block py-2 px-4">
+            <a href="https://twitter.com/mirko_960">Twitter</a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
