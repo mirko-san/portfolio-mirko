6121e9292ee8d7db2ee83f2f8cbb977a21ea7bc7
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-10-01 20:08:25 +0900

    :sparkles: テーマにカラー設定を追加

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,12 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    colors: {
+      base: "#f8f3de",
+      primary: "#d4dfbb",
+      secondary: "#e5af9b",
+      white: "#ffffff",
+    },
+  },
 } as Options;
