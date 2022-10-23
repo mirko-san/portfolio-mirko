58b46c39d45062eb159147815d4164405a77d746
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-10-23 22:26:01 +0900

    :recycle: className -> class

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,27 +6,27 @@ const cssIcon = css({
 
 export default function Home() {
   return (
-    <div className="p-4 mx-auto max-w-screen-md">
-      <h2 className="text-5xl text-center mt-8">Profile</h2>
+    <div class="p-4 mx-auto max-w-screen-md">
+      <h2 class="text-5xl text-center mt-8">Profile</h2>
       <section>
-        <div className="flex justify-center mt-8">
+        <div class="flex justify-center mt-8">
           {/* XXX: CSSのエラーはあとで直す */}
           <img
-            className="bg-white max-w-[80%] rounded-full object-fill border-8 border-secondary"
+            class="bg-white max-w-[80%] rounded-full object-fill border-8 border-secondary"
             src="/profile_image.png"
             alt="mirko-san profile image"
           />
         </div>
 
-        <div className="flex justify-center mt-8">
-          <p className="text-5xl">
+        <div class="flex justify-center mt-8">
+          <p class="text-5xl">
             <span>ミルコ ( mirko )</span>
             <span class={tw(cssIcon)} className="pl-2">&#xf0000;</span>
           </p>
         </div>
 
-        <div className="flex justify-center mt-4">
-          <ul className="list-disc">
+        <div class="flex justify-center mt-4">
+          <ul class="list-disc">
             <li>
               <span>セルフ受肉勢の VTuber です。</span>
             </li>
@@ -38,8 +38,8 @@ export default function Home() {
       </section>
 
       <section>
-        <div className="flex justify-center mt-4">
-          <div className="inline-block py-2 px-4">
+        <div class="flex justify-center mt-4">
+          <div class="inline-block py-2 px-4">
             <a
               href="https://twitter.com/mirko_960"
               target="_blank"
@@ -62,7 +62,7 @@ export default function Home() {
               <span class="underline">Twitter</span>
             </a>
           </div>
-          <div className="inline-block py-2 px-4 ml-2 underline">
+          <div class="inline-block py-2 px-4 ml-2 underline">
             <a
               href="https://www.youtube.com/channel/UCwoEKcxzNJ_VUW0TPiPfyqw"
               target="_blank"
